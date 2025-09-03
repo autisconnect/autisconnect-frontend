2aa685fe8b94671a0beab961eb8bfb3078d35663
autisconnect <autisconnect@gmail.com>
2025-09-03 18:36:34 -0300

    aiven3.8

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from './Login';
 import Signup from './Signup';
 import ParentDashboard from './ParentDashboard';
 import ProfessionalDashboard from './ProfessionalDashboard';
-import FinancialDashboard from './FinancialDashboard'; // <-- IMPORTAÇÃO ADICIONADA
+import FinancialDashboard from './FinancialDashboard';
 import SecretaryDashboard from './SecretaryDashboard';
 import ServiceDashboard from './ServiceDashboard';
 import ServiceDashboard01 from './service_dashboard/ServiceDashboard01';
@@ -19,8 +19,8 @@ import EmotionTrackingDashboard from './emotion-tracking/EmotionTrackingDashboar
 import StrokeRiskMonitor from './StrokeRiskMonitor';
 import StereotypyMonitor from './StereotypyMonitor';
 import TriggerRecorder from './TriggerRecorder';
-import PatientDetails from './PatientDetails'; // Novo componente para detalhes do paciente
-import { AuthProvider } from './context/AuthContext.jsx';
+import PatientDetails from './PatientDetails';
+import { AuthProvider } from './context/AuthContext';
 import PresentationServiceDashboard from './presentation-dashboard/PresentationServiceDashboard';
 import PresentationProfessionalDashboard from './presentation-dashboard/PresentationProfessionalDashboard';
 import PresentationParentDashboard from './presentation-dashboard/PresentationParentDashboard';
@@ -35,11 +35,8 @@ import PresentationStereotypyMonitor from './presentation-dashboard/Presentation
 import PresentationSecretaryDashboard from './presentation-dashboard/PresentationSecretaryDashboard';
 import PaymentSuccess from './PaymentSuccess';
 import PaymentFailure from './PaymentFailure';
-
-
 import './App.css';
 
-// Componente para renderizar o dashboard público correto com base no ID
 const PublicServiceDashboard = () => {
   const { id } = useParams();
   switch (id) {
@@ -52,7 +49,6 @@ const PublicServiceDashboard = () => {
   }
 };
 
-// Componente para renderizar o dashboard de sessões com userId dinâmico
 const DynamicSessionsGraph = () => {
   const { userId } = useParams();
   return <SessionsGraph userId={userId ? parseInt(userId) : 2} />;
@@ -63,12 +59,9 @@ function App() {
     <AuthProvider>
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
-          {/* Rotas públicas */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-
-          {/* Rotas de apresentação */}
           <Route path="/presentation" element={<PresentationServiceDashboard />} />
           <Route path="/PresentationProfessionalDashboard" element={<PresentationProfessionalDashboard />} />
           <Route path="/PresentationParentDashboard" element={<PresentationParentDashboard />} />
@@ -81,78 +74,58 @@ function App() {
           <Route path="/presentation-dashboard/PresentationTriggerRecorder" element={<PresentationTriggerRecorder />} />
           <Route path="/presentation-dashboard/PresentationStereotypyMonitor" element={<PresentationStereotypyMonitor />} />
           <Route path="/presentation-dashboard/PresentationSecretaryDashboard" element={<PresentationSecretaryDashboard />} />
-        
-          {/* Rotas protegidas para dashboards */}
           <Route
             path="/parent-dashboard/:id"
-            element={<ProtectedRoute allowedUserType="pais_responsavel"><ParentDashboard /></ProtectedRoute>}
+            element={<ProtectedRoute allowedUserTypes={['pais_responsavel']}><ParentDashboard /></ProtectedRoute>}
           />
           <Route
             path="/professional-dashboard/:id"
-            element={<ProtectedRoute allowedUserType="medicos_terapeutas"><ProfessionalDashboard /></ProtectedRoute>}
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas']}><ProfessionalDashboard /></ProtectedRoute>}
           />
-
           <Route
             path="/financial-dashboard/:id"
-            element={
-              <ProtectedRoute allowedUserType="medicos_terapeutas">
-                <FinancialDashboard />
-              </ProtectedRoute>
-            }
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas']}><FinancialDashboard /></ProtectedRoute>}
           />
-
           <Route
-              path="/secretary-dashboard/:id"
-              element={<ProtectedRoute allowedUserType="secretaria"><SecretaryDashboard /></ProtectedRoute>}
+            path="/secretary-dashboard/:id"
+            element={<ProtectedRoute allowedUserTypes={['secretaria']}><SecretaryDashboard /></ProtectedRoute>}
           />
-
           <Route
             path="/service-dashboard"
-            element={<ProtectedRoute allowedUserType="servicos_locais"><ServiceDashboard /></ProtectedRoute>}
+            element={<ProtectedRoute allowedUserTypes={['servicos_locais']}><ServiceDashboard /></ProtectedRoute>}
           />
           <Route
             path="/service-dashboard/:id"
-            element={<ProtectedRoute allowedUserType="servicos_locais"><ServiceDashboard /></ProtectedRoute>}
+            element={<ProtectedRoute allowedUserTypes={['servicos_locais']}><ServiceDashboard /></ProtectedRoute>}
           />
-
-          {/* Rota para detalhes do paciente */}
           <Route
             path="/patient-details/:patientId"
-            element={<ProtectedRoute allowedUserType="medicos_terapeutas"><PatientDetails /></ProtectedRoute>}
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas']}><PatientDetails /></ProtectedRoute>}
           />
-
-          {/* Rotas protegidas para ferramentas específicas */}
-            <Route
-              path="/emotion-detector"
-              element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><EmotionDetector /></ProtectedRoute>}
-            />
-            <Route
-              path="/stroke-risk-monitor"
-              element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><StrokeRiskMonitor /></ProtectedRoute>}
-            />
-            <Route
-              path="/stereotypy-monitor"
-              element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><StereotypyMonitor /></ProtectedRoute>}
-            />
-            <Route
-              path="/trigger-recorder"
-              element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><TriggerRecorder /></ProtectedRoute>}
-            />
-          
-          {/* Rotas para ferramentas de monitoramento e análise */}
-          <Route path="/emotion-detector" element={<EmotionDetector />} />
+          <Route
+            path="/emotion-detector"
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><EmotionDetector /></ProtectedRoute>}
+          />
+          <Route
+            path="/stroke-risk-monitor"
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><StrokeRiskMonitor /></ProtectedRoute>}
+          />
+          <Route
+            path="/stereotypy-monitor"
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><StereotypyMonitor /></ProtectedRoute>}
+          />
+          <Route
+            path="/trigger-recorder"
+            element={<ProtectedRoute allowedUserTypes={['medicos_terapeutas', 'pais_responsavel']}><TriggerRecorder /></ProtectedRoute>}
+          />
           <Route path="/emotion-graph" element={<EmotionChart />} />
           <Route path="/sessions-graph" element={<SessionsGraph userId={2} />} />
           <Route path="/sessions-graph/:userId" element={<DynamicSessionsGraph />} />
           <Route path="/emotion-dashboard" element={<EmotionTrackingDashboard />} />
-          <Route path="/stroke-risk-monitor" element={<StrokeRiskMonitor />} />
-
-          {/* Rotas públicas para páginas de serviços específicos */}
           <Route path="/service_dashboard/ServiceDashboard:id" element={<PublicServiceDashboard />} />
           <Route path="/service_dashboard/ServiceDashboard01" element={<ServiceDashboard01 />} />
           <Route path="/service_dashboard/ServiceDashboard17" element={<ServiceDashboard17 />} />
           <Route path="/service_dashboard/ServiceDashboard18" element={<ServiceDashboard18 />} />
-
           <Route path="/payment-success" element={<PaymentSuccess />} />
           <Route path="/payment-failure" element={<PaymentFailure />} />
         </Routes>
