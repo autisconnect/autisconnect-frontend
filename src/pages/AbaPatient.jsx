7a6e2bda74a004ab179912ace4262c8252e28761
autisconnect <autisconnect@gmail.com>
2026-01-19 12:40:24 -0300

    19/01/2026 - 02

--- src/pages/AbaPatient.jsx
+++ src/pages/AbaPatient.jsx
@@ -13,7 +13,7 @@ import AbaAiAnalysis from '../components/AbaAiAnalysis';
 // import AbaProgramSuggestions from '../components/AbaProgramSuggestions';
 // import AbaProgramMonitoring from '../components/AbaProgramMonitoring';
 
-import '../app.css';
+import './App.css';
 
 const AbaPatient = ({ patientId }) => {
     const [activeTab, setActiveTab] = useState('dashboard');
